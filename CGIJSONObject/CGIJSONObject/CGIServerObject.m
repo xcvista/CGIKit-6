3e81745bfb4b1c78f1a02551a779434e882a082d
Maxthon Chan <xcvista@me.com>
2013-08-16 02:36:04 +0800

    CGIServerObject as CGIContextHandler

--- CGIJSONObject/CGIJSONObject/CGIServerObject.m
+++ CGIJSONObject/CGIJSONObject/CGIServerObject.m
@@ -7,12 +7,39 @@
 //
 
 #import "CGIServerObject.h"
+#import "NSObject+CGIJSONSerializable.h"
 
 @implementation CGIServerObject
 
 - (id)objectFromProcessingContext:(CGIHTTPContext *)context
 {
     return nil;
+}
+
+#pragma mark - HTTP Content Delegate
+
+- (BOOL)context:(CGIHTTPContext *)context shouldHandleRequest:(CGIHTTPRequest *)request
+{
+    id object = [NSJSONSerialization JSONObjectWithData:request.data
+                                                options:0
+                                                  error:NULL];
+    return [self setSerializedObject:object];
+}
+
+- (void)handleHTTPContext:(CGIHTTPContext *)context
+{
+    id response = [self objectFromProcessingContext:context];
+    
+    if (response)
+    {
+        NSData *responseData = [NSJSONSerialization dataWithJSONObject:[response serializedObject]
+                                                               options:0
+                                                                 error:NULL];
+        if (responseData)
+            [context.response.data setData:response];
+    }
+    
+    context.response.contentType = @"application/json";
 }
 
 @end
