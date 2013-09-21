74fa2a1815ebaa2d5b89456a490e3ab207cb0244
Maxthon Chan <xcvista@me.com>
2013-09-21 12:03:04 +0800

    6.0 RC4

--- CGIJSONObject/CGIJSONObject/CGIRemoteConnection.h
+++ CGIJSONObject/CGIJSONObject/CGIRemoteConnection.h
@@ -33,67 +33,70 @@ extern NSString *const CGIRemoteConnectionServerRootKey;
 @interface CGIRemoteConnection : NSObject
 
 /**
- 服务器地址根。
+ Serer address root.
  
- @note      格式如下：http://server/path/to/script?%@，也即，以“%@”作为方法名
-            占位符。
+ The format of this string should be like: http://server/path/to/script?%@ -
+ that is, use %@ as a placeholder of the method name.
+ 
  @see       -initWithServerRoot:
  */
 @property NSString *serverRoot;
 
+/**
+ Connection delegate.
+ */
 @property id<CGIRemoteConnectionDelegate> delegate;
 
 /**
- 获得默认远程连接控制对象。
+ Get the default remote connection object.
  
- @return    默认远程连接控制对象。
+ @return    The default remote connection object.
  @see       -makeDefaultServerRoot
  */
 + (instancetype)defaultRemoteConnection;
 
 /**
- 以指定的服务器根地址初始化远程连接控制对象。
+ Initialize the connection with a given address root.
  
- @param     serverRoot  服务器根地址。
- @return    初始化的远程连接控制对象。
+ @param     serverRoot  Server address root.
  @see       serverRoot
  */
 - (id)initWithServerRoot:(NSString *)serverRoot;
 
 /**
- 使当前对象成为默认远程连接对象。
+ Make the receiver the default remote connection.
  
  @see       +defaultRemoteConnection
  */
 - (void)makeDefaultServerRoot;
 
 /**
- 向指定远程方法发送请求数据，并得到返回数据。
+ Send data to the remote service, asking for response data.
  
- @param     data    请求数据。
- @param     method  远程方法名。
- @param     error   如果调用失败，指针所指向的对象将被修改为错误的详细信息。您可
-                    传入 NULL 屏蔽这一输出。
+ @param     data    Request data
+ @param     method  Name of the remote method
+ @param     error   If the call fails, this pointer will point to an object
+                    telling the reason of failure.
  */
 - (NSData *)dataWithData:(NSData *)data
               fromMethod:(NSString *)method
                    error:(NSError *__autoreleasing *)error;
 
 /**
- 获取用户代理字符串。
- 
- @return    用户代理字符串。
+ Get the user agent string.
  */
 - (NSString *)userAgent;
 
 /**
- 从方法名得到 URL 请求对象。
+ Get the URL request from name of the method.
  
- @param     method  远程方法名。
- @return    URL 请求对象。
+ @param     method  Name of the method
  */
 - (NSMutableURLRequest *)URLRequestForMethod:(NSString *)method;
 
+/**
+ Ask the delegate if the request should be sent.
+ */
 - (BOOL)connectionShouldSendURLRequest:(NSMutableURLRequest *)request;
 
 @end
