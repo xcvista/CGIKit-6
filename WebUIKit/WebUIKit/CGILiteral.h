7c5affabf91e94cee6b9f2045c6b4aa954cbb7ab
Maxthon Chan <xcvista@me.com>
2013-10-10 11:35:02 +0800

    Literal: the most important control ever.

---
+++ WebUIKit/WebUIKit/CGILiteral.h
@@ -0,0 +1,15 @@
+//
+//  CGILiteral.h
+//  WebUIKit
+//
+//  Created by Maxthon Chan on 10/10/13.
+//
+//
+
+#import <WebUIKit/CGIView.h>
+
+@interface CGILiteral : CGIView
+
+@property NSString *content;
+
+@end
---
+++ WebUIKit/WebUIKit/CGILiteral.m
@@ -0,0 +1,18 @@
+//
+//  CGILiteral.m
+//  WebUIKit
+//
+//  Created by Maxthon Chan on 10/10/13.
+//
+//
+
+#import "CGILiteral.h"
+
+@implementation CGILiteral
+
+- (NSString *)HTMLRepresentation
+{
+    return self.content;
+}
+
+@end
